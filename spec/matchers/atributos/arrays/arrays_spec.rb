484577171fcc21dd55c78d99824655d503d1eb20
guimelo03 <guilhermemelo03@hotmail.com>
2025-09-24 16:50:57 -0300

    mocks, test_doubles e stubs

--- spec/matchers/atributos/arrays/arrays_spec.rb
+++ spec/matchers/atributos/arrays/arrays_spec.rb
@@ -1,14 +1,14 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]), "Array" do
+describe Array.new([1,2,3]), "Array", type: 'collection' do
     it "#include" do
         expect(subject).to include(2)
         expect(subject).to include(2,1)
     end
 
-    it { expect(subject).to an_array_excluding(2,1) }
+    it { expect(subject).to exclude(4) }
 
-    it '#contain_exactly' do
+    it '#contain_exactly', :slow do
         expect(subject).to contain_exactly(1,2,3)
     end
 
@@ -16,4 +16,3 @@ describe Array.new([1,2,3]), "Array" do
         expect(subject).to match_array([1,2,3])
     end
 end
-
--- spec/matchers/satisfy/satisfy_spec.rb
+++ spec/matchers/satisfy/satisfy_spec.rb
@@ -1,7 +1,7 @@
-descibe 'satisfy' do
-    it { expect(10).to satisfy { |x| x % 2 == 0 }}
-    it { 
-        expect(9).to satisfy('be a multiple part of 3') |x|
-         x % 3 == 0 }
+describe 'satisfy' do
+    it { expect(10).to satisfy { |x| x % 2 == 0 } }
+
+    it 'should satisfy the condition with a custom description' do
+        expect(9).to satisfy('be a multiple of 3') { |x| x % 3 == 0 }
     end
 end
